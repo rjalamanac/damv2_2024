a83fbb7ed73a204a042836ec0954b76ccddc39fb
rjalamanac <rjalamanac@iescomercio.com>
2025-01-23 22:45:27 +0100

    Hacks and stuff

--- AngularBase/src/app/components/housing-location/housing-location.component.ts
+++ AngularBase/src/app/components/housing-location/housing-location.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../../models/housinglocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-  
+
   @Input() housingLocation!: HousingLocation;
-  
+
 }
